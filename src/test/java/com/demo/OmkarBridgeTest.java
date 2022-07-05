b66e0f473ede7d0989424c04a559c52721a6a23e
Onkar <60186332+onkar26ers@users.noreply.github.com>
2022-07-05 18:38:26 +0530

    Add files via upload

---
+++ src/test/java/com/demo/OmkarBridgeTest.java
@@ -0,0 +1,11 @@
+package com.demo;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+public class OmkarBridgeTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
