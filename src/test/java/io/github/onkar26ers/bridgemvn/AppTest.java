d791b2e47519a50155e7b48342fbf6b8e7b312de
Onkar <60186332+onkar26ers@users.noreply.github.com>
2022-07-05 19:36:07 +0530

    Add files via upload

---
+++ src/test/java/io/github/onkar26ers/bridgemvn/AppTest.java
@@ -0,0 +1,20 @@
+package io.github.onkar26ers.bridgemvn;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
